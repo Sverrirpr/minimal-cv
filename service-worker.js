ef8d18bdbc9cf4562861eb607225f8fee185c8e8
Sverrir <spr1@nemandi.ntv.is>
2019-10-08 21:37:30 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sverrirpr/minimal-cv.git/precache-manifest.ba86328cc8c21ed94f9d2fe62f5024a1.js"
+  "/minimal-cv/precache-manifest.5e3b8e1e659e1941a30181468e88bd94.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sverrirpr/minimal-cv.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minimal-cv/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
