0c7fbe6198f355cedbcbbb810b32787a5971f594
Sverrir <spr1@nemandi.ntv.is>
2019-10-08 21:08:42 +0000

    Updates

---
+++ precache-manifest.ba86328cc8c21ed94f9d2fe62f5024a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099259992d7318a489629378a49e5688",
+    "url": "/Sverrirpr/minimal-cv.git/index.html"
+  },
+  {
+    "revision": "8c89efd3e82c28579051",
+    "url": "/Sverrirpr/minimal-cv.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a0cbb544160e6635a389",
+    "url": "/Sverrirpr/minimal-cv.git/static/js/2.11012d8f.chunk.js"
+  },
+  {
+    "revision": "8c89efd3e82c28579051",
+    "url": "/Sverrirpr/minimal-cv.git/static/js/main.0517f314.chunk.js"
+  },
+  {
+    "revision": "8d466a3affee759cae96",
+    "url": "/Sverrirpr/minimal-cv.git/static/js/runtime-main.1c4e58c4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Sverrirpr/minimal-cv.git/static/media/logo.25bf045c.svg"
+  }
+]);
